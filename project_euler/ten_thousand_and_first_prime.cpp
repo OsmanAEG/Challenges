8491cafa385b4fc40f935ef135c9a87487285acf
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-06-28 06:22:38 -0400

    ten thousand and first prime

--- project_euler/ten_thousand_and_first_prime.cpp
+++ project_euler/ten_thousand_and_first_prime.cpp
@@ -1,9 +1,10 @@
-// Project Euler: 3
-// Largest Prime Factor
+// Project Euler: 7
+// 10 001st Prime
 
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <string>
 
 template<typename Int_T>
 bool is_prime(const Int_T num) {
@@ -18,14 +19,21 @@ bool is_prime(const Int_T num) {
 
 int main() {
   using Int_T = unsigned long long;
-  Int_T largest = 1;
-  Int_T num = 600851475143;
 
-  for(Int_T i = 2; i < std::sqrt(num); ++i) {
-    if(num%i == 0 && is_prime(i)) largest = i;
+  Int_T prime_num = 2;
+  Int_T max_it = 10001;
+
+  int it = 1;
+
+  while(it < max_it) {
+    ++prime_num;
+
+    while(!is_prime(prime_num)) ++prime_num;
+
+    ++it;
   }
 
-  std::cout << largest << std::endl;
+  std::cout << prime_num << std::endl;
 
   return 0;
 }
