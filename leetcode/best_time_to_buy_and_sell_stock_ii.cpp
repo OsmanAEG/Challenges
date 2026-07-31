9fb4bbf2e37935d3777615d67ca30f9f6f890aa8
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-07-31 12:32:32 -0400

    best time to buy and sell stock ii

---
+++ leetcode/best_time_to_buy_and_sell_stock_ii.cpp
@@ -0,0 +1,19 @@
+// LeetCode: 122
+// Best Time to Buy and Sell Stock II
+
+#include<algorithm>
+#include<climits>
+#include<vector>
+
+class Solution {
+public:
+  int maxProfit(std::vector<int>& prices) {
+    int profit = 0;
+
+    for(int i = 1; i < prices.size(); ++i) {
+      if(prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+    }
+
+    return profit;
+  }
+};
