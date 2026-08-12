32bbf642d17c7bceb60d017e6b2193ab59ba85c4
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-08-12 13:32:28 -0400

    pandigital multiples

---
+++ project_euler/pandigital_multiples.cpp
@@ -0,0 +1,49 @@
+// Project Euler: 38
+// Pandigital Multiples
+
+#include "helper.h"
+
+using Int_T = unsigned long long;
+using String_T = std::string;
+
+bool is_pandigital(String_T num) {
+  String_T pandigits = "123456789";
+
+  std::sort(num.begin(), num.end());
+
+  if(num == pandigits) return true;
+
+  return false;
+}
+
+int main() {
+  Int_T num_limit = 10000;
+  Int_T result_limit = 9;
+
+  Int_T n = 1;
+  Int_T num = 2;
+
+  Int_T max = 0;
+
+  while(num < num_limit) {
+    Int_T product = num;
+    std::string result = "";
+
+    while(result.size() < result_limit) {
+      product = num*n;
+      result += std::to_string(product);
+      ++n;
+
+      if(is_pandigital(result)) {
+        max = std::max(max, std::stoull(result));
+      }
+    }
+
+    n = 1;
+    ++num;
+  }
+
+  std::cout << max << std::endl;
+
+  return 0;
+}
